0cce05959f03e697660eab166ee3d69aa21b6812
İsmail Can Karataş <ismailcankaratasss@email.com>
2025-09-07 19:09:27 +0300

    Refactor project structure and update app configuration
    - Renamed app from "movie_app" to "MovieFlix" and updated related assets.
    - Introduced Tailwind CSS for styling with a new configuration file.
    - Added new components for search functionality and tab navigation.
    - Removed unused components and assets to streamline the project.
    - Updated package dependencies, including the addition of nativewind and tailwindcss.
    - Implemented a new onboarding screen and movie details page.
    - Enhanced the layout and design of the main application screens.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,81 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { icons } from '@/constants/icons'
+import { images } from '@/constants/images'
+import { Tabs } from 'expo-router'
+import React from 'react'
+import { Image, ImageBackground, ImageSourcePropType, Text, View } from 'react-native'
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+interface TabIconProps {
+  focused: boolean
+  icon: ImageSourcePropType
+  label: string
 }
+
+const TabIcon = ({ focused, icon, label }: TabIconProps) => {
+  if (focused) {
+    return (
+      <ImageBackground source={images.highlight} className='flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 items-center justify-center rounded-full overflow-hidden'>
+        <Image source={icon} tintColor="#151312" className='size-5' />
+        <Text className='text-secondary text-base font-semibold ml-2'>{label}</Text>
+      </ImageBackground>
+    )
+  }
+  return (
+    <View className='size-full mt-4 rounded-full items-center justify-center'>
+      <Image source={icon} tintColor="#A8B5DB" />
+    </View>
+  )
+}
+
+const TabsLayout = () => {
+  return (
+    <Tabs screenOptions={{
+      tabBarShowLabel: false,
+      tabBarItemStyle: {
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      tabBarStyle: {
+        backgroundColor: '#0f0D23',
+        borderRadius: 50,
+        marginHorizontal: 20,
+        marginBottom: 36,
+        height: 52,
+        position: 'absolute',
+        overflow: 'hidden',
+        borderWidth: 1,
+        borderColor: '#0f0d23',
+      }
+    }}>
+      <Tabs.Screen name="index" options={{
+        title: 'Home',
+        tabBarIcon: ({ focused }) => (
+          <TabIcon focused={focused} icon={icons.home} label="Home" />
+        ),
+        headerShown: false
+      }} />
+      <Tabs.Screen name="search" options={{
+        title: 'Search', tabBarIcon: ({ focused }) => (
+          <TabIcon focused={focused} icon={icons.search} label="Search" />
+        ),
+        headerShown: false
+      }} />
+      <Tabs.Screen name="saved" options={{
+        title: 'Saved', tabBarIcon: ({ focused }) => (
+          <TabIcon focused={focused} icon={icons.save} label="Saved" />
+        ),
+        headerShown: false
+      }} />
+      <Tabs.Screen name="profile" options={{
+        title: 'Profile', tabBarIcon: ({ focused }) => (
+          <TabIcon focused={focused} icon={icons.person} label="Profile" />
+        ),
+        headerShown: false
+      }} />
+    </Tabs>
+  )
+}
+
+export default TabsLayout
+
