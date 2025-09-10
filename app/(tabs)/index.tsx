24ded190ba365f20e30eede23019f5da195a549f
İsmail Can Karataş <ismailcankaratasss@email.com>
2025-09-10 20:27:46 +0300

    Add trending movies feature and new dependency
    - Integrated trending movies functionality in the main tab index, fetching data from Appwrite.
    - Added a new TrendingCard component for displaying trending movies.
    - Updated SearchBar component to make value and onChangeText optional.
    - Included @react-native-masked-view/masked-view dependency in package.json and package-lock.json for enhanced UI rendering.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,22 @@
 import MovieCard from '@/components/MovieCard';
 import SearchBar from '@/components/SearchBar';
+import TrendingCard from '@/components/TrendingCard';
 import { icons } from '@/constants/icons';
 import { images } from '@/constants/images';
 import { fetchMovies } from '@/services/api';
+import { getTrendingMovies } from '@/services/appwrite';
 import useFetch from '@/services/useFetch';
 import { useRouter } from 'expo-router';
 import { ActivityIndicator, FlatList, Image, ScrollView, Text, View } from 'react-native';
 
 export default function Index() {
   const router = useRouter();
+
+  const {
+    data: trendingMovies,
+    loading: trendingMoviesLoading,
+    error: trendingMoviesError
+  } = useFetch<TrendingMovie[]>(() => getTrendingMovies())
 
   const {
     data: movies,
@@ -26,20 +34,43 @@ export default function Index() {
         contentContainerStyle={{ minHeight: '100%', paddingBottom: 100 }}>
         <Image source={icons.logo} className="w-12 h-10 mt-20 m-5 mx-auto" />
 
-        {moviesLoading ? (
+        {moviesLoading  || trendingMoviesLoading ? (
           <ActivityIndicator
             size='large'
             color='#0000ff'
             className='mt-10 self-center'
           />
-        ) : moviesError ? (
-          <Text className='text-red-500'>Error: {moviesError?.message}</Text>
+        ) : moviesError || trendingMoviesError ? (
+          <Text className='text-red-500'>Error: {moviesError?.message || trendingMoviesError?.message}</Text>
         ) : (
           <View className="flex-1 mt-5">
             <SearchBar
               placeholder="Search for a movie"
               onPress={() => router.push('/search')}
             />
+
+            {trendingMovies && trendingMovies.length > 0 && (
+              <>
+              <View className='mt-10'>
+                <Text className='text-lg text-white font-bold mb-3'>Trending Movies</Text>
+              </View>
+
+                <FlatList
+                  horizontal
+                  showsHorizontalScrollIndicator={false}
+                  ItemSeparatorComponent={() => <View className='w-4' />}
+                  className='mb-4 mt-3'
+                  data={trendingMovies}
+                  renderItem={({ item, index}) => (
+                    <TrendingCard movie={item} index={index} />
+                  )}
+                  keyExtractor={(item) => item.movie_id.toString()}
+                />
+
+
+              </>
+              
+            )}
 
             <View className='flex-1'>
               <Text className='text-lg text-white font-bold mt-5 mb-3'>Latest Movies</Text>
